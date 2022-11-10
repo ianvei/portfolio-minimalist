20c1bf99dd037581b65bda892f73d001e40df6e2
ianvei <iamianveilleux@gmail.com>
2022-11-10 11:57:12 -0700

    vite scaffold

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app style="flex-direction:column; align-items:center ;" flex-direction="column" align-items="center">
+    <v-main class="" max-width="48rem">
+      <HeaderVueVue />
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import HeaderVueVue from './components/HeaderVue.vue';
+</script>
---
+++ src/components/HeaderVue.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container class="">
+    <v-row no-gutters>
+      <v-col>
+        <div class="text-h1 align-lg-start">Ian</div>
+        <div class="text-h1">Veilleux</div>
+      </v-col>
+      <v-col class="align-lg-end">
+        <div class="hdr-images">
+          <img :src="ianveiIcon" alt="" class="ianveiIcon">
+        </div>
+        <div class="hdr-text">
+          <p class="font-weight-regular mb-2">Normal weight text.</p>
+          <p class="font-weight-regular">Normal weight text.</p>
+          <p class="font-weight-regular">Normal weight text.</p>
+          <p class="font-weight-regular">Normal weight text.</p>
+        </div>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import ianveiIcon from '../assets/ianveiicon.png'
+</script>
+
+<style scoped>
+  .ianveiIcon{
+    max-width: 3rem;
+  }
+</style>
